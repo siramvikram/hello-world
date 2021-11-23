0a61953bbb6104ee7c830df18f5241c96c40f786
Siram <siramv@c02dr04fmd6v.lan>
2021-11-23 17:55:59 -0500

    initial commit

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello world",
+	})
+}
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/", IndexHandler)
+	router.Run()
+}
